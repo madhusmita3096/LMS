1d1f6c0b2a5eda8356849f21720094deeb80e89a
madhusmita3096 <madhusmita3096@gmail.com>
2024-11-29 11:08:28 +0530

    lms commit

---
+++ lms_api/main/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from . import models
+
+class TeacherSerializer(serializers.ModelSerializer):
+
+    class Meta:
+       model=models.Teacher
+       fields=['id','full_name','email','password','qualification','mobile_no','skills']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+       model=models.CourseCategory
+       fields=['id','title','description']
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+       model=models.Course
+       #fields = '_all_' 
+       fields=['id','category','teacher','title','description','featured_img','techs']
